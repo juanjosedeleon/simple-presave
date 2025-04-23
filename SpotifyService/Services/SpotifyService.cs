528431bcc501b633d2f7f1f206dcaca01d642420
Juan De Leon <jdeleon@apexsystems.com>
2025-04-23 13:21:11 -0600

    Refactor GetToken method for access token and refresh token

--- SpotifyService/Services/SpotifyService.cs
+++ SpotifyService/Services/SpotifyService.cs
@@ -7,6 +7,12 @@ namespace SimplePresave.Libraries.Services;
 
 public class SpotifyService
 {
+    public class GrantType
+    {
+        public const string AUTHORIZATION_CODE = "authorization_code";
+        public const string REFRESH_TOKEN = "refresh_token";
+    }
+
     public DateTimeOffset PublishDate => _publishDate;
     private readonly string _clientId;
     private readonly string _clientSecret;
@@ -26,42 +32,9 @@ public class SpotifyService
 
     public async Task<Dictionary<string, string>> RequestAccessToken(string authorizationCode, string redirectUri)
     {
-        var tokenUrl = "https://accounts.spotify.com/api/token";
-        var authHeader = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{_clientId}:{_clientSecret}"));
-        var requestBody = new FormUrlEncodedContent(new[]
-        {
-            new KeyValuePair<string, string>("grant_type", "authorization_code"),
-            new KeyValuePair<string, string>("code", authorizationCode),
-            new KeyValuePair<string, string>("redirect_uri", redirectUri)
-        });
+        var result = await GetToken(GrantType.AUTHORIZATION_CODE, authorizationCode, redirectUri);
 
-        var request = new HttpRequestMessage(HttpMethod.Post, tokenUrl)
-        {
-            Content = requestBody
-        };
-        request.Headers.Add("Authorization", $"Basic {authHeader}");
-
-        var response = await _httpClient.SendAsync(request);
-
-        if (!response.IsSuccessStatusCode)
-        {
-            throw new Exception($"Fallo al obtener access token. Código de estatus: {response.StatusCode}");
-        }
-
-        var responseContent = await response.Content.ReadAsStringAsync();
-        var responseData = JsonSerializer.Deserialize<SpotifyTokenResponse>(responseContent);
-
-        if (responseData == null || string.IsNullOrEmpty(responseData.access_token) || string.IsNullOrEmpty(responseData.refresh_token))
-        {
-            throw new Exception("Respuesta inválida desde la Spotify API. Faltan access_token o refresh_token.");
-        }
-
-        return new Dictionary<string, string>
-        {
-            { "access_token", responseData.access_token },
-            { "refresh_token", responseData.refresh_token },
-            { "expires_in", responseData.expires_in.ToString() }
-        };
+        return result;
     }
 
     public async Task<string> GetUserEmail(string accessToken)
@@ -86,7 +59,7 @@ public class SpotifyService
     public async Task AddSongToLibrary(PresaveMessage message)
     {
         var trackUrl = "https://api.spotify.com/v1/me/tracks";
-        var accessToken = await RefreshAccessToken(message.RefreshToken);
+        var accessToken = await GetNewAccessToken(message.RefreshToken);
         var requestBody = new StringContent(JsonSerializer.Serialize(new { ids = new[] { $"{_trackId}" } }), Encoding.UTF8, "application/json");
         var request = new HttpRequestMessage(HttpMethod.Put, trackUrl)
         {
@@ -100,32 +73,66 @@ public class SpotifyService
         }
     }
 
-    private async Task<string> RefreshAccessToken(string refreshToken)
+    private async Task<string> GetNewAccessToken(string refreshToken)
+    {
+        var result = await GetToken(GrantType.REFRESH_TOKEN, refreshToken);
+
+        return result["access_token"];
+    }
+
+    private async Task<Dictionary<string, string>> GetToken(string grantType, string tokenOrAuthCode, string redirectUri = "")
     {
         var tokenUrl = "https://accounts.spotify.com/api/token";
         var authHeader = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{_clientId}:{_clientSecret}"));
-        var requestBody = new FormUrlEncodedContent(new[]
+
+        KeyValuePair<string, string>[] requestParameters;
+        switch (grantType)
         {
-            new KeyValuePair<string, string>("grant_type", "refresh_token"),
-            new KeyValuePair<string, string>("refresh_token", refreshToken)
-        });
+            case GrantType.AUTHORIZATION_CODE:
+                requestParameters = new[]
+                {
+                    new KeyValuePair<string, string>("grant_type", GrantType.AUTHORIZATION_CODE),
+                    new KeyValuePair<string, string>("code", tokenOrAuthCode),
+                    new KeyValuePair<string, string>("redirect_uri", redirectUri)
+                };
+                break;
+            case GrantType.REFRESH_TOKEN:
+                requestParameters = new[]
+                {
+                    new KeyValuePair<string, string>("grant_type", GrantType.REFRESH_TOKEN),
+                    new KeyValuePair<string, string>("refresh_token", tokenOrAuthCode)
+                };
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(grantType), grantType, null);
+        }
+
+        var requestBody = new FormUrlEncodedContent(requestParameters);
         var request = new HttpRequestMessage(HttpMethod.Post, tokenUrl)
         {
             Content = requestBody
         };
         request.Headers.Add("Authorization", $"Basic {authHeader}");
         var response = await _httpClient.SendAsync(request);
+
         if (!response.IsSuccessStatusCode)
         {
             throw new Exception($"Fallo al obtener access token. Código de estatus: {response.StatusCode}");
         }
+
         var responseContent = await response.Content.ReadAsStringAsync();
         var responseData = JsonSerializer.Deserialize<SpotifyTokenResponse>(responseContent);
+
         if (responseData == null || string.IsNullOrEmpty(responseData.access_token))
         {
             throw new Exception("Respuesta inválida desde la Spotify API. Falta access_token.");
         }
 
-        return responseData.access_token;
+        return new Dictionary<string, string>
+        {
+            { "access_token", responseData.access_token },
+            { "refresh_token", responseData.refresh_token },
+            { "expires_in", responseData.expires_in.ToString() }
+        };
     }
 }
